f864c0f33779533ea6018fa529b8c01074277f22
NIcholas Staples <nicholas.staples@solution-tribute.com>
2020-08-16 17:04:31 +0100

    Update zip_submission.py
    Required create zip.

--- zip_submission.py
+++ zip_submission.py
@@ -3,8 +3,8 @@ from zipfile import ZipFile
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-nn_path = os.path.join(current_dir, 'mnist', 'src', 'nn.py')
-spam_path = os.path.join(current_dir, 'spam', 'src', 'spam.py')
+nn_path = os.path.join(current_dir, 'src', 'mnist', 'nn.py')
+spam_path = os.path.join(current_dir, 'src', 'spam', 'spam.py')
 
 zipObj = ZipFile(os.path.join(current_dir, 'PS4_submission.zip'), 'w')
 zipObj.write(nn_path, os.path.basename(nn_path))
